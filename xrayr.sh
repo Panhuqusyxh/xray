76019c13dd56e4e3b68e0605ed903eb6c9dd2123
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2023-12-30 01:03:04 +0700

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -1,1 +1,23 @@
-wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh v0.8.8
+#!/bin/bash
+
+# Kiểm tra sự tồn tại của file xrayrpro.sh
+
+wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh v0.8.7
+
+
+if [ -e "xrayrpro.sh" ]; then
+    echo "Đã tìm thấy xrayrpro.sh. Đang xóa..."
+    rm xrayrpro.sh
+    echo "Đã xóa xrayrpro.sh"
+else
+    echo "Không tìm thấy xrayrpro.sh"
+fi
+
+# Kiểm tra sự tồn tại của file xrayr.sh
+if [ -e "xrayr.sh" ]; then
+    echo "Đã tìm thấy xrayr.sh. Đang xóa..."
+    rm xrayr.sh
+    echo "Đã xóa xrayr.sh"
+else
+    echo "Không tìm thấy xrayr.sh"
+fi

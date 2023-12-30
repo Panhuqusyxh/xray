0db4d5016022fe130aee2213c9c2f6811f14c9ee
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2023-12-30 10:17:51 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Tìm và xóa file 1.sh, a.sh, b.sh nếu tồn tại
+files_to_delete=("1.sh" "a.sh" "b.sh")
+
+for file in "${files_to_delete[@]}"; do
+    if [ -e "$file" ]; then
+        rm "$file"
+        echo "Đã xóa file $file."
+    else
+        echo "File $file không tồn tại."
+    fi
+done
+
+
 read -p "Chọn tên người dùng (
 1: dualeo Enter để sử dụng mặc định dualeo
 2: thanh ): " choice

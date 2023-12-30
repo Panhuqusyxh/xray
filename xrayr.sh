f45b215ab86ceec611f551f980e02af320ff94d7
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2023-12-30 13:20:04 +0700

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -2,7 +2,7 @@
 
 # Kiểm tra sự tồn tại của file xrayrpro.sh
 
-wget -N https://github.com/Panhuqusyxh/xray/blob/main/xrayr1.sh && bash install.sh
+wget -N https://github.com/Panhuqusyxh/xray/blob/main/xrayr1.sh && bash xrayr1.sh
 
 
 if [ -e "xrayrpro.sh" ]; then

bc85dbe6638d7f76f35af83f14f8c2ab9ee95f39
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-23 02:49:50 +0700

    Create ddns.sh

---
+++ ddns.sh
@@ -0,0 +1,10 @@
+# Cấu hình thông tin CloudFlare DDNS
+
+
+cloudflare-ddns --configure << EOF
+Y
+K
+dcmnmmmchkh@gmail.com
+3b411374ee6b120fbfc87be4b80e930922034
+svn.dualeovpn.net,aws1.dualeovpn.net,aws2.dualeovpn.net,aws3.dualeovpn.net,aws4.dualeovpn.net,aws5.dualeovpn.net,aws6.dualeovpn.net
+EOF

e9a081999232d4a1bcfc16931966a0cf5e07bba4
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-24 01:31:41 +0700

    Update az.sh

--- az.sh
+++ az.sh
@@ -5,6 +5,10 @@ if [ "$EUID" -ne 0 ]; then
     echo "Bạn không đang ở root, hãy đăng nhập vào tài khoản root để thực hiện lệnh này."
     exit 1
 fi
+
+
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/gost_auto.sh)
+
 # add bbr 
 wget sh.alhttdw.cn/d11.sh && bash d11.sh
 

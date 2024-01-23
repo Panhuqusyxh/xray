bd94a84c6cf5c69c38e0aeba0d9472cb08c299ac
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-24 01:18:40 +0700

    Update gost_auto.sh

--- gost_auto.sh
+++ gost_auto.sh
@@ -12,7 +12,7 @@ chmod 777 gost
 
 # Chạy gost
 nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &
-nohup ./gost -L udp://:10004 -L tcp://:10004 -F relay+tls://sv.dualeovpn.net:20004 >> /dev/null 2>&1 &4
+nohup ./gost -L udp://:10004 -L tcp://:10004 -F relay+tls://sv.dualeovpn.net:20004 >> /dev/null 2>&1 &
 
 # tạo tệp cron
 

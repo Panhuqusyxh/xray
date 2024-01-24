61cd4d1ac198d134cba5f98c2d8d486b7d5b9ef2
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-24 23:40:33 +0700

    Update az.sh

--- az.sh
+++ az.sh
@@ -51,7 +51,7 @@ cloudflare-ddns --configure << EOF
 K
 dcmnmmmchkh@gmail.com
 3b411374ee6b120fbfc87be4b80e930922034
-svn.dualeovpn.net,aws1.dualeovpn.net,aws2.dualeovpn.net,aws3.dualeovpn.net,aws4.dualeovpn.net,aws5.dualeovpn.net,aws6.dualeovpn.net
+svn.dualeovpn.net,aws1.dualeovpn.net,aws2.dualeovpn.net,aws3.dualeovpn.net,aws4.dualeovpn.net,aws5.dualeovpn.net,aws6.dualeovpn.net,aws.dualeovpn.net
 EOF
 
 

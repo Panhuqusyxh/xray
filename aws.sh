6dc329e54859cc254b179f37aba3431086a2de2a
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-27 13:51:40 +0700

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,12 +1,12 @@
-
-echo "Chọn mã server (A, B, C, D, E):  "
+echo -n "Chọn mã server (A, B, C, D, E):  "
 read option
+option=$(echo "$option" | tr '[:upper:]' '[:lower:]')  # Chuyển đổi thành chữ thường
 
 case $option in
-  A) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws1.sh);;
-  B) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws2.sh);;
-  C) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws3.sh);;
-  D) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws4.sh);;
-  E) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws5.sh);;
+  a) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws1.sh);;
+  b) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws2.sh);;
+  c) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws3.sh);;
+  d) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws4.sh);;
+  e) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws5.sh);;
   *) echo "đã cài"; exit 1;;
 esac

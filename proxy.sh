b1aa2e20c2a4ea1ae5ac93d0d11dc7319d1cef85
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-28 02:02:33 +0700

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid3-install.sh &&  sudo bash squid3-install.sh
+sudo squid-add-user
+aaa
+aaa
+aaa
+apt install net-tools 

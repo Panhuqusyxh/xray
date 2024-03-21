8e7e02e8f2f18cd8cab0ce9dc835aa4fed12c584
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-03-21 18:29:14 +0700

    Update CT-TT.sh

--- CT-TT.sh
+++ CT-TT.sh
@@ -1,3 +1,58 @@
+#!/bin/bash
+
+# Kiểm tra xem người dùng hiện tại có phải là root không
+if [ "$EUID" -ne 0 ]; then
+    echo "Bạn không đang ở root, hãy đăng nhập vào tài khoản root để thực hiện lệnh này."
+    exit 1
+fi
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
+# update 
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y nano wget curl
+# thay pass
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
+# add bbr 
+wget sh.alhttdw.cn/d11.sh && bash d11.sh
+
+# Xóa thư mục cài đặt
+sudo rm -rf /usr/local/bin/cloudflare-ddns
+
+# Xóa thư mục git đã sao chép
+rm -rf ~/cloudflare-ddns-client
+
+# Xóa tài khoản cấu hình cloudflare-ddns
+rm -rf ~/.cloudflare-ddns
+
+# Xóa tệp cấu hình tùy chọn nếu có
+rm -f ~/.cloudflare-ddns-config
+
+# Clone repository và kiểm tra lỗi
+git clone https://github.com/LINKIWI/cloudflare-ddns-client.git && cd cloudflare-ddns-client || {
+    echo "Lỗi khi clone repository. Kiểm tra kết nối mạng của bạn."
+    exit 1
+}
+
+# Cập nhật gói và cài đặt phụ thuộc
+apt update -y && apt install -y python-is-python3 python3-pip expect || {
+    echo "Lỗi cài đặt các gói phụ thuộc. Kiểm tra kết nối mạng và quyền của bạn."
+    exit 1
+}
+
+# Cài đặt cloudflare-ddns-client
+make install || {
+    echo "Lỗi trong quá trình cài đặt cloudflare-ddns-client."
+    exit 1
+}
+
+# Cấu hình thông tin CloudFlare DDNS
+cloudflare-ddns --configure << EOF
+K
+dcmnmmmchkh@gmail.com
+3b411374ee6b120fbfc87be4b80e930922034
+3.thanhtong.top,aws.dualeovpn.net
+EOF
+
+
 # Tải Gost
 wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
 

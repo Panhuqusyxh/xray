ac554ebb53bc652d84a4b317277461e1df7398fd
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-03-21 18:51:39 +0700

    Update CT-TT.sh

--- CT-TT.sh
+++ CT-TT.sh
@@ -5,6 +5,9 @@ if [ "$EUID" -ne 0 ]; then
     echo "Bạn không đang ở root, hãy đăng nhập vào tài khoản root để thực hiện lệnh này."
     exit 1
 fi
+
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/gost_auto.sh)
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/gost_auto.sh)
 bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
 bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
 # update 
@@ -49,52 +52,29 @@ cloudflare-ddns --configure << EOF
 K
 dcmnmmmchkh@gmail.com
 3b411374ee6b120fbfc87be4b80e930922034
-aws.dualeovpn.net
+3.thanhtong.top,aws.dualeovpn.net,tt.dualeovpn.net
 EOF
 
 
-# Tải Gost
-wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
-
-# Giải nén Gost
-gzip -d gost-linux-amd64-2.11.5.gz
-
-# Đổi tên tệp thực thi
-mv gost-linux-amd64-2.11.5 gost
-
-# Cấp quyền thực thi cho tệp Gost
-chmod 777 gost
-
-# Chạy gost
-nohup ./gost -L udp://:10080 -L tcp://:10080 -F relay+tls://sv.dualeovpn.net:20080 >> /dev/null 2>&1 &
-nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &
-# tạo tệp cron
-
-sudo touch gost_auto.sh
-echo '#!/bin/bash' > gost_auto.sh
-echo 'nohup ./gost -L udp://:10080 -L tcp://:10080 -F relay+tls://sv.dualeovpn.net:20080 >> /dev/null 2>&1 &' >> gost_auto.sh
-echo 'nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &' >> gost_auto.sh
-
-# cấp quyền 
-
-sudo chmod 777 gost_auto.sh
 
 
-# Ghi tác vụ cron thứ hai vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
-echo "@reboot /root/gost_auto.sh" >> /root/cloudflare_cron
-echo "*/1 * * * * /root/gost_auto.sh" >> /root/cloudflare_cron
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/gost_auto.sh)
 
-# Ghi tác vụ cron thứ ba vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
-echo "0 * * * * rm -f /root/ipcf.log" >> /root/cloudflare_cron
+# Cài xrayr 
+bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/xrayr1.sh)
 
-# Nhập tất cả tác vụ cron từ tệp tạm thời
-crontab /root/cloudflare_cron
+# Đường dẫn tới tệp cấu hình XrayR
+config_file="/etc/XrayR/config.yml"
 
-# Xóa tệp tạm thời
-rm /root/cloudflare_cron
+# Xóa nội dung của tệp cấu hình
+echo -n "" > "$config_file"
 
+# Lấy nội dung từ URL và thêm vào tệp cấu hình
+curl -sSfL "https://raw.githubusercontent.com/Panhuqusyxh/xray/main/80.txt" >> "$config_file"
 
-ps aux | grep gost
+# Kết thúc thông báo
+echo "Nội dung của $config_file đã được cập nhật từ URL."
+xrayr restart
 clear
 # add vps lên vps.dualeovpn.net
 bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/add-Nezha.sh)

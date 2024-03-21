35ad059c70339be0531ac391e9bb837623ab29ac
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-03-21 18:22:03 +0700

    Update gost_auto.sh

--- gost_auto.sh
+++ gost_auto.sh
@@ -13,17 +13,12 @@ chmod 777 gost
 # Chạy gost
 nohup ./gost -L udp://:10080 -L tcp://:10080 -F relay+tls://sv.dualeovpn.net:20080 >> /dev/null 2>&1 &
 nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &
-nohup ./gost -L udp://:10004 -L tcp://:10004 -F relay+tls://sv.dualeovpn.net:20004 >> /dev/null 2>&1 &
-nohup ./gost -L udp://:10095 -L tcp://:10095 -F relay+tls://sv.dualeovpn.net:20095 >> /dev/null 2>&1 &
-
 # tạo tệp cron
 
 sudo touch gost_auto.sh
 echo '#!/bin/bash' > gost_auto.sh
 echo 'nohup ./gost -L udp://:10080 -L tcp://:10080 -F relay+tls://sv.dualeovpn.net:20080 >> /dev/null 2>&1 &' >> gost_auto.sh
 echo 'nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &' >> gost_auto.sh
-echo 'nohup ./gost -L udp://:10004 -L tcp://:10004 -F relay+tls://sv.dualeovpn.net:20004 >> /dev/null 2>&1 &' >> gost_auto.sh
-echo 'nohup ./gost -L udp://:10095 -L tcp://:10095 -F relay+tls://sv.dualeovpn.net:20095 >> /dev/null 2>&1 &' >> gost_auto.sh
 
 # cấp quyền 
 

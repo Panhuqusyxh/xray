d51efc0924a1ef8ed2ecb896e64683bdf30adae2
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-03-21 18:22:55 +0700

    Update CT-TT.sh

--- CT-TT.sh
+++ CT-TT.sh
@@ -1,96 +1,42 @@
-#!/bin/bash
+# Tải Gost
+wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
 
-# Kiểm tra xem người dùng hiện tại có phải là root không
-if [ "$EUID" -ne 0 ]; then
-    echo "Bạn không đang ở root, hãy đăng nhập vào tài khoản root để thực hiện lệnh này."
-    exit 1
-fi
-bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
-bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
-# update 
-sudo apt update -y && sudo apt upgrade -y && sudo apt install -y nano wget curl
-# thay pass
-bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/change-pass.sh)
-# add bbr 
-wget sh.alhttdw.cn/d11.sh && bash d11.sh
+# Giải nén Gost
+gzip -d gost-linux-amd64-2.11.5.gz
 
-# Xóa thư mục cài đặt
-sudo rm -rf /usr/local/bin/cloudflare-ddns
+# Đổi tên tệp thực thi
+mv gost-linux-amd64-2.11.5 gost
 
-# Xóa thư mục git đã sao chép
-rm -rf ~/cloudflare-ddns-client
+# Cấp quyền thực thi cho tệp Gost
+chmod 777 gost
 
-# Xóa tài khoản cấu hình cloudflare-ddns
-rm -rf ~/.cloudflare-ddns
+# Chạy gost
+nohup ./gost -L udp://:10080 -L tcp://:10080 -F relay+tls://sv.dualeovpn.net:20080 >> /dev/null 2>&1 &
+nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &
+# tạo tệp cron
 
-# Xóa tệp cấu hình tùy chọn nếu có
-rm -f ~/.cloudflare-ddns-config
+sudo touch gost_auto.sh
+echo '#!/bin/bash' > gost_auto.sh
+echo 'nohup ./gost -L udp://:10080 -L tcp://:10080 -F relay+tls://sv.dualeovpn.net:20080 >> /dev/null 2>&1 &' >> gost_auto.sh
+echo 'nohup ./gost -L udp://:10066 -L tcp://:10066 -F relay+tls://sv.dualeovpn.net:20066 >> /dev/null 2>&1 &' >> gost_auto.sh
 
-# Clone repository và kiểm tra lỗi
-git clone https://github.com/LINKIWI/cloudflare-ddns-client.git && cd cloudflare-ddns-client || {
-    echo "Lỗi khi clone repository. Kiểm tra kết nối mạng của bạn."
-    exit 1
-}
+# cấp quyền 
 
-# Cập nhật gói và cài đặt phụ thuộc
-apt update -y && apt install -y python-is-python3 python3-pip expect || {
-    echo "Lỗi cài đặt các gói phụ thuộc. Kiểm tra kết nối mạng và quyền của bạn."
-    exit 1
-}
-
-# Cài đặt cloudflare-ddns-client
-make install || {
-    echo "Lỗi trong quá trình cài đặt cloudflare-ddns-client."
-    exit 1
-}
-
-# Cấu hình thông tin CloudFlare DDNS
-cloudflare-ddns --configure << EOF
-K
-dcmnmmmchkh@gmail.com
-3b411374ee6b120fbfc87be4b80e930922034
-aws.dualeovpn.net
-EOF
+sudo chmod 777 gost_auto.sh
 
 
+# Ghi tác vụ cron thứ hai vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
+echo "@reboot /root/gost_auto.sh" >> /root/cloudflare_cron
+echo "*/1 * * * * /root/gost_auto.sh" >> /root/cloudflare_cron
+
+# Ghi tác vụ cron thứ ba vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
+echo "0 * * * * rm -f /root/ipcf.log" >> /root/cloudflare_cron
+
+# Nhập tất cả tác vụ cron từ tệp tạm thời
+crontab /root/cloudflare_cron
+
+# Xóa tệp tạm thời
+rm /root/cloudflare_cron
 
 
-
-# Cài xrayr 
-bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/xrayr1.sh)
-
-# Đường dẫn tới tệp cấu hình XrayR
-config_file="/etc/XrayR/config.yml"
-
-# Xóa nội dung của tệp cấu hình
-echo -n "" > "$config_file"
-
-# Lấy nội dung từ URL và thêm vào tệp cấu hình
-curl -sSfL "https://raw.githubusercontent.com/Panhuqusyxh/xray/main/80.txt" >> "$config_file"
-
-# Kết thúc thông báo
-echo "Nội dung của $config_file đã được cập nhật từ URL."
-xrayr restart
-clear
-# add vps lên vps.dualeovpn.net
-bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/add-Nezha.sh)
-
-# Thực hiện cập nhật DDNS ngay lập tức
-cloudflare-ddns --update-now
-
-# auto reboot
- bash <(curl -Ls  https://raw.githubusercontent.com/Panhuqusyxh/xray/main/reboot.sh)
-
-clear 
-echo -e "\e[30;48;5;82mCài xong AZ\e[0m Lên WEB"
-#!/bin/bash
-# khởi động lại 
-echo "Bạn có muốn khởi động lại VPS không? (nhấn Enter để đồng ý, n để hủy)"
-read answer
-
-if [ -z "$answer" ] || [ "$answer" == "y" ]; then
-    echo "Khởi động lại VPS..."
-    sudo reboot
-else
-    echo "Không khởi động lại VPS."
-fi
+ps aux | grep gost
